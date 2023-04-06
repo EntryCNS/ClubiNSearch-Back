1f981ca7605ca8824354df39c0e9709a880ea5c7
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-06 13:46:31 +0900

    :tada: :: setup project

---
+++ src/main/java/com/dgsw/cns/clubinsearch/ClubiNSearchApplication.java
@@ -0,0 +1,14 @@
+package com.dgsw.cns.clubinsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubiNSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClubiNSearchApplication.class, args);
+    }
+
+}
+    
---
+++ src/test/java/com/dgsw/cns/clubinsearch/ClubiNSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.cns.clubinsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubiNSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

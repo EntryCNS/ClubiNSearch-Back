c75d158bebc9b8b2976a5eb102990f236bdbfbff
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-10 15:36:47 +0900

    :recycle: :: 불필요한 코드 삭제

--- src/main/java/com/dgsw/cns/clubinsearch/domain/recruitment/domain/repository/RecruitmentRepository.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/recruitment/domain/repository/RecruitmentRepository.java
@@ -1,12 +1,8 @@
 package com.dgsw.cns.clubinsearch.domain.recruitment.domain.repository;
 
 import com.dgsw.cns.clubinsearch.domain.recruitment.domain.Recruitment;
+import com.dgsw.cns.clubinsearch.domain.recruitment.domain.repository.custom.RecruitmentCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
-
-    List<Recruitment> findByClub_IdAndTitleAndPositionAndEmploymentType(Long id, String search, String position, String employmentType);
-
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>, RecruitmentCustomRepository {
 }

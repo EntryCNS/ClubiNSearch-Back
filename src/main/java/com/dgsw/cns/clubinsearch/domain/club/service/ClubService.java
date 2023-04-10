cf32625dc383bac6efb873ee5d18be40e1a1d195
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-10 11:26:22 +0900

    :bookmark_tabs: :: club service

---
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/club/service/ClubService.java
@@ -0,0 +1,30 @@
+package com.dgsw.cns.clubinsearch.domain.club.service;
+
+import com.dgsw.cns.clubinsearch.domain.club.domain.Club;
+import com.dgsw.cns.clubinsearch.domain.club.domain.repository.ClubRepository;
+import com.dgsw.cns.clubinsearch.domain.club.exception.ClubNameExistsException;
+import com.dgsw.cns.clubinsearch.domain.club.presentation.dto.RegisterClubRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClubService {
+
+    private final ClubRepository clubRepository;
+
+    public void registerClub(RegisterClubRequest request) {
+
+        if(clubRepository.existsByName(request.getClubName())) {
+            throw ClubNameExistsException.EXCEPTION;
+        }
+
+        Club club = Club.builder()
+                .name(request.getClubName())
+                .build();
+
+        clubRepository.save(club);
+
+    }
+
+}

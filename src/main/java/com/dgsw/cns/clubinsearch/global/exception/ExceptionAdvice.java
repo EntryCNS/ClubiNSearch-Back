f37efbf9e2730d4b5a3ca8c5c640642a988be03c
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-10 11:17:19 +0900

    :sparkles: :: create exception advice

---
+++ src/main/java/com/dgsw/cns/clubinsearch/global/exception/ExceptionAdvice.java
@@ -0,0 +1,27 @@
+package com.dgsw.cns.clubinsearch.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ExceptionResponse> businessException(BusinessException e) {
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(new ExceptionResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionResponse(e.getAllErrors().get(0).getDefaultMessage()));
+    }
+}
---
+++ src/main/java/com/dgsw/cns/clubinsearch/global/exception/ExceptionResponse.java
@@ -0,0 +1,10 @@
+package com.dgsw.cns.clubinsearch.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ExceptionResponse {
+    private String message;
+}

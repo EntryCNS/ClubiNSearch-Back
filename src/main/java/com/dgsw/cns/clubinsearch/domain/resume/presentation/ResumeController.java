ff360f908948e911a168bc228aab83846fbb5b46
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-11 13:38:07 +0900

    :bookmark_tabs: :: resume controller

---
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/resume/presentation/ResumeController.java
@@ -0,0 +1,7 @@
+package com.dgsw.cns.clubinsearch.domain.resume.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResumeController {
+}

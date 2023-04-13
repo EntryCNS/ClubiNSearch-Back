9f43ae661841253eec23d879c498de889ec287d7
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-13 10:26:25 +0900

    :recycle: :: 안쓰는 import 삭제

--- src/main/java/com/dgsw/cns/clubinsearch/domain/recruitment/domain/repository/custom/RecruitmentCustomRepositoryImpl.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/recruitment/domain/repository/custom/RecruitmentCustomRepositoryImpl.java
@@ -4,7 +4,6 @@ import com.dgsw.cns.clubinsearch.domain.recruitment.domain.Recruitment;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 

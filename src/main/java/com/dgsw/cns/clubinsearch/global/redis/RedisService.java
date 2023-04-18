e48010ccb6c2f25d48ce637847c3f39222562005
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-18 17:58:15 +0900

    :sparkles: :: redis service

---
+++ src/main/java/com/dgsw/cns/clubinsearch/global/redis/RedisService.java
@@ -0,0 +1,21 @@
+package com.dgsw.cns.clubinsearch.global.redis;
+
+import com.dgsw.cns.clubinsearch.domain.auth.domain.RefreshToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate redisTemplate;
+
+    public void save(RefreshToken refreshToken) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        valueOperations.set(refreshToken.getRefreshToken(), refreshToken.getEmail());
+        redisTemplate.expire(refreshToken.getRefreshToken(), 3600L, TimeUnit.SECONDS);
+    }
+}

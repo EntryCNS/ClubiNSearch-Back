15f7e2887089b37005602911bbf37a060fd4097a
pea06 <101560095+pea06@users.noreply.github.com>
2023-04-30 15:53:53 +0900

    :necktie: :: update s3 image upload logic

--- src/main/java/com/dgsw/cns/clubinsearch/thirdparty/s3/service/S3Service.java
+++ src/main/java/com/dgsw/cns/clubinsearch/thirdparty/s3/service/S3Service.java
@@ -3,6 +3,7 @@ package com.dgsw.cns.clubinsearch.thirdparty.s3.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
 import com.dgsw.cns.clubinsearch.thirdparty.s3.enums.Dir;
 import com.dgsw.cns.clubinsearch.thirdparty.s3.exception.FileUploadFailedException;
 import com.dgsw.cns.clubinsearch.thirdparty.s3.properties.S3Properties;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
@@ -19,8 +21,6 @@ import java.util.UUID;
 public class S3Service {
     private final AmazonS3Client amazonS3Client;
     private final S3Properties s3Properties;
-
-    private final AmazonS3 amazonS3;
 
     public String uploadFile(
         Dir dir,
@@ -33,10 +33,17 @@ public class S3Service {
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
         objectMetadata.setContentType(multipartFile.getContentType());
+        objectMetadata.setContentLength(multipartFile.getSize());
 
-        try (InputStream inputStream = multipartFile.getInputStream()) {
-            amazonS3Client.putObject(new PutObjectRequest(s3Properties.getBucket(), fileName, inputStream, objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        try {
+
+            PutObjectRequest request = new PutObjectRequest(
+                    s3Properties.getBucket(),
+                    fileName,
+                    multipartFile.getInputStream(),
+                    objectMetadata
+            );
+            amazonS3Client.putObject(request.withCannedAcl(CannedAccessControlList.Private));
         } catch (IOException e) {
             throw FileUploadFailedException.Exception;
         }

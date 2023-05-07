40cb568df695c852d4f9954b27cf0a3bf4a073dc
pea06 <101560095+pea06@users.noreply.github.com>
2023-05-07 20:25:08 +0900

    :recycle: :: update redis config

--- src/main/java/com/dgsw/cns/clubinsearch/global/redis/RedisConfig.java
+++ src/main/java/com/dgsw/cns/clubinsearch/global/redis/RedisConfig.java
@@ -1,17 +1,13 @@
 package com.dgsw.cns.clubinsearch.global.redis;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.stereotype.Component;
-
 
 @Configuration
 @EnableRedisRepositories
@@ -23,7 +19,10 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(redisHost);
+        redisStandaloneConfiguration.setPort(redisPort);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean

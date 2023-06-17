c6d077c904ea5e73e5c451fc01a69fa0ea26b599
pea06 <101560095+pea06@users.noreply.github.com>
2023-06-17 22:08:00 +0900

    :zap: :: separation method

--- src/main/java/com/dgsw/cns/clubinsearch/domain/resume/presentation/AdminResumeController.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/resume/presentation/AdminResumeController.java
@@ -1,10 +1,14 @@
 package com.dgsw.cns.clubinsearch.domain.resume.presentation;
 
 import com.dgsw.cns.clubinsearch.domain.resume.presentation.dto.request.UpdateResumeStateRequest;
+import com.dgsw.cns.clubinsearch.domain.resume.presentation.dto.response.ResumeResponse;
+import com.dgsw.cns.clubinsearch.domain.resume.service.QueryResumeListService;
 import com.dgsw.cns.clubinsearch.domain.resume.service.UpdateResumeStateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping ("/api/resume/admin")
@@ -12,9 +16,21 @@ import org.springframework.web.bind.annotation.*;
 public class AdminResumeController {
     private final UpdateResumeStateService updateResumeStateService;
 
-    @PutMapping("/state")
+    private final QueryResumeListService queryResumeListService;
+
+    @GetMapping("/list/{recruitmentId}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ResumeResponse> getResumeListByRecruitmentId(
+            @PathVariable("recruitmentId") Long recruitmentId
+    ) {
+        return queryResumeListService.execute(recruitmentId);
+    }
+
+    @PostMapping("/state")
     @ResponseStatus(HttpStatus.OK)
     public void updateResumeState(@RequestBody UpdateResumeStateRequest request) {
         updateResumeStateService.execute(request);
     }
+
+
 }
--- src/main/java/com/dgsw/cns/clubinsearch/domain/resume/presentation/ResumeController.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/resume/presentation/ResumeController.java
@@ -1,35 +1,23 @@
 package com.dgsw.cns.clubinsearch.domain.resume.presentation;
 
 import com.dgsw.cns.clubinsearch.domain.resume.presentation.dto.request.SubmitResumeRequest;
-import com.dgsw.cns.clubinsearch.domain.resume.presentation.dto.response.ResumeResponse;
-import com.dgsw.cns.clubinsearch.domain.resume.service.ResumeService;
+import com.dgsw.cns.clubinsearch.domain.resume.service.SumbitResumeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/resume")
 @RequiredArgsConstructor
 public class ResumeController {
 
-    private final ResumeService resumeService;
+    private final SumbitResumeService sumbitResumeService;
 
     @PostMapping("/submit")
     @ResponseStatus(HttpStatus.CREATED)
     public void submitResume(
             @ModelAttribute SubmitResumeRequest request) {
-        resumeService.submitResume(request);
-    }
-
-    @GetMapping("/list/{recruitmentId}")
-    @ResponseStatus(HttpStatus.OK)
-    public List<ResumeResponse> getResumeListByRecruitmentId(
-            @PathVariable("recruitmentId") Long recruitmentId
-    ) {
-        return resumeService.getResumeListByRecruitmentId(recruitmentId);
+        sumbitResumeService.execute(request);
     }
 
 }

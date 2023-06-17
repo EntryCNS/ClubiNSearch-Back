d60a0addacc1fdfb2ec62f9f72223ba71628459f
pea06 <101560095+pea06@users.noreply.github.com>
2023-06-17 22:06:30 +0900

    :zap: :: separation method

--- src/main/java/com/dgsw/cns/clubinsearch/domain/resume/service/QueryResumeListService.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/resume/service/QueryResumeListService.java
@@ -1,54 +1,24 @@
 package com.dgsw.cns.clubinsearch.domain.resume.service;
 
-import com.dgsw.cns.clubinsearch.domain.recruitment.domain.Recruitment;
-import com.dgsw.cns.clubinsearch.domain.recruitment.domain.repository.RecruitmentRepository;
-import com.dgsw.cns.clubinsearch.domain.recruitment.exception.RecruitmentNotFoundException;
 import com.dgsw.cns.clubinsearch.domain.resume.domain.Resume;
 import com.dgsw.cns.clubinsearch.domain.resume.domain.enums.State;
 import com.dgsw.cns.clubinsearch.domain.resume.domain.repository.ResumeRepository;
 import com.dgsw.cns.clubinsearch.domain.resume.exception.ResumeListEmptyException;
-import com.dgsw.cns.clubinsearch.domain.resume.presentation.dto.request.SubmitResumeRequest;
 import com.dgsw.cns.clubinsearch.domain.resume.presentation.dto.response.ResumeResponse;
-import com.dgsw.cns.clubinsearch.thirdparty.s3.enums.Dir;
-import com.dgsw.cns.clubinsearch.thirdparty.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
-public class ResumeService {
+public class QueryResumeListService {
 
     private final ResumeRepository resumeRepository;
 
-    private final RecruitmentRepository recruitmentRepository;
-
-    private final S3Service s3Service;
-    public void submitResume(SubmitResumeRequest request) {
-        Recruitment recruitment = recruitmentRepository.findById(request.getRecruitmentId())
-                .orElseThrow(() -> RecruitmentNotFoundException.EXCEPTION);
-
-        Resume resume = Resume.builder()
-                .name(request.getName())
-                .studentNo(request.getStudentNo())
-                .contact(request.getContact())
-                .introduction(request.getIntroduction())
-                .link(request.getLink())
-                .fileUrl(s3Service.uploadFile(Dir.RESUME, request.getFile()))
-                .state(State.SUBMIT)
-                .build();
-
-        recruitment.addResume(resume);
-
-        resumeRepository.save(resume);
-    }
-
-    public List<ResumeResponse> getResumeListByRecruitmentId(Long recruitmentId) {
+    public List<ResumeResponse> execute(Long recruitmentId) {
         List<Resume> resumeList = resumeRepository.findAllByRecruitment_Id(recruitmentId);
         if (resumeList.isEmpty()) {
             throw ResumeListEmptyException.EXCEPTION;

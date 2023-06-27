9f44cf5854b2fd017855d7ff870d8f1afc11a0f1
pea06 <101560095+pea06@users.noreply.github.com>
2023-06-27 20:45:52 +0900

    :sparkles: :: add new club apis

--- src/main/java/com/dgsw/cns/clubinsearch/domain/club/presentation/ClubController.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/club/presentation/ClubController.java
@@ -1,10 +1,13 @@
 package com.dgsw.cns.clubinsearch.domain.club.presentation;
 
-import com.dgsw.cns.clubinsearch.domain.club.presentation.dto.RegisterClubRequest;
+import com.dgsw.cns.clubinsearch.domain.club.presentation.dto.request.AddClubProfileRequest;
+import com.dgsw.cns.clubinsearch.domain.club.presentation.dto.request.RegisterClubRequest;
+import com.dgsw.cns.clubinsearch.domain.club.presentation.dto.response.ClubProfileResponse;
+import com.dgsw.cns.clubinsearch.domain.club.service.AddClubProfileService;
 import com.dgsw.cns.clubinsearch.domain.club.service.ClubService;
+import com.dgsw.cns.clubinsearch.domain.club.service.QueryClubProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -16,12 +19,32 @@ public class ClubController {
 
     private final ClubService clubService;
 
+    private final AddClubProfileService addClubProfileService;
+
+    private final QueryClubProfileService queryClubProfileService;
+
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerClub(
             @RequestBody @Valid RegisterClubRequest request
             ) {
         clubService.registerClub(request);
+    }
+
+    @PostMapping("/profile")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addProfile(
+            @ModelAttribute AddClubProfileRequest request
+            ) {
+        addClubProfileService.execute(request);
+    }
+
+    @GetMapping("/profile/{clubId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ClubProfileResponse getProfile(
+            @PathVariable("clubId") Long clubId
+            ) {
+        return queryClubProfileService.execute(clubId);
     }
 
 }

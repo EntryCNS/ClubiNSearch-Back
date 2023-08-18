8b72bf8e3381a3b70a97e29d298690a7aaffd53b
pea06 <101560095+pea06@users.noreply.github.com>
2023-08-18 20:58:36 +0900

    :card_file_box: :: 임시로 유저 터이블에 동아리 추가

--- src/main/java/com/dgsw/cns/clubinsearch/domain/user/domain/User.java
+++ src/main/java/com/dgsw/cns/clubinsearch/domain/user/domain/User.java
@@ -1,15 +1,15 @@
 package com.dgsw.cns.clubinsearch.domain.user.domain;
 
+import com.dgsw.cns.clubinsearch.domain.club.domain.Club;
 import com.dgsw.cns.clubinsearch.domain.user.domain.enums.Role;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) @AllArgsConstructor
+@Builder
 @Getter
 public class User {
     @Id
@@ -24,16 +24,22 @@ public class User {
 
     private String password;
 
+    @OneToOne
+    private Club club;
+
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Builder
-    public User(Long id, String accountId, String name, String email, String password, Role role) {
-        this.id = id;
-        this.accountId = accountId;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return false;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return Objects.equals(getId(), user.getId());
     }
 }
